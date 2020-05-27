ae42b1c902f7b0b16964f900425ef9e6c63004a4
shinhyogeun <tls1gy2rms3@naver.com>
2020-05-27 23:12:41 +0900

    [ add cell form]

--- WhatYouWant/ChoiceViewController.swift
+++ WhatYouWant/ChoiceViewController.swift
@@ -15,13 +15,13 @@ class ChoiceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainTableView.dataSource = self
-        
         self.mainTableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "cell1")
+        self.mainTableView.rowHeight = 100
     }
     
 }
 
-extension ChoiceViewController : UITableViewDataSource {
+extension ChoiceViewController : UITableViewDataSource,UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 100
@@ -31,5 +31,7 @@ extension ChoiceViewController : UITableViewDataSource {
         let cell = mainTableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! MainTableViewCell
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{return 100}
     
 }
---
+++ WhatYouWant/Data/RawMaterials.swift
@@ -0,0 +1,9 @@
+//
+//  RawMaterials.swift
+//  WhatYouWant
+//
+//  Created by 신효근 on 2020/05/26.
+//  Copyright © 2020 신효근. All rights reserved.
+//
+
+import Foundation
